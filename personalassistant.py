6bfb81d93761f4248754d1afcbb36ba268d033c7
Badica Ioana Maria <117526248+BadicaIoana@users.noreply.github.com>
2023-01-30 15:35:21 +0200

    Update personalassistant.py

--- personalassistant.py
+++ personalassistant.py
@@ -3,54 +3,62 @@ import requests
 from datetime import datetime
 import pyttsx3
 
-r = sr.Recognizer ( )
+# Recognizer instance
+r = sr.Recognizer()
 
-while True :
-	engine = pyttsx3.init ( )
-	with sr.Microphone ( ) as source :
-		audio = r.listen ( source )
-	try :
-		try :
-			request = input ( "You: " )  # get input from the user via the terminal
-		
-		except :
-			request = r.recognize_google ( audio , language = "en-US" )
-		print ( "You: " + request )
-		# Check if user greeted
-		if "hello" in request.lower ( ) :
-			response = "Hello! How can I help you?"
-		# Check if user is asking how the assistant is doing
-		elif "how are you" in request.lower ( ) :
-			response = "I am doing well, thank you for asking!"
-		# Check if user is asking who the assistant is
-		elif "what are you" in request.lower ( ) :
-			response = "I'm Ghost, your personal assistant."
-		elif "what is the date" in request.lower ( ) :
-			response = f"Today is {datetime.now ( ).strftime ( '%m/%d/%Y' )}"
-		elif "what is the time" in request.lower ( ) :
-			response = f"The current time is {datetime.now ( ).strftime ( '%H:%M:%S' )}"
-		elif "what's the weather" in request.lower ( ) :
-			# get weather information from OpenWeatherMap API
-			api_key = 'your api'
-			base_url = "http://api.openweathermap.org/data/2.5/weather?"
-			city_name = "your city"  # replace this with the desired city
-			complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-			response = requests.get ( complete_url )
-			x = response.json ( )
-			if x [ "cod" ] != "404" :
-				y = x [ "main" ]
-				current_temperature = y [ "temp" ]
-				current_pressure = y [ "pressure" ]
-				current_humidity = y [ "humidity" ]
-				z = x [ "weather" ]
-				weather_description = z [ 0 ] [ "description" ]
-				response = f"Temperature: {current_temperature}°C\nPressure: {current_pressure} hPa\nHumidity: {current_humidity}%\nWeather description: {weather_description}"
-			else :
-				response = "City not found"
-		else :
-			response = "I don't understand what you want to say. Can I help you with anything else?"
-	except sr.UnknownValueError :
-		response = "I didn't understand what you said."
-	print ( "Assistant:" , response )
-	engine.say ( response )
-	engine.runAndWait ( )
+# Infinite loop for processing user input
+while True:
+    # Initialize text-to-speech engine
+    engine = pyttsx3.init()
+    
+    # Use microphone as audio source
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+
+    # Try to recognize user input
+    try:
+        # If user provided input via terminal
+        try:
+            request = input("You: ")  
+        except:
+            # If user provided input via speech
+            request = r.recognize_google(audio, language="en-US")
+        print("You: " + request)
+        
+        # Respond based on the user input
+        if "hello" in request.lower():
+            response = "Hello! How can I help you?"
+        elif "how are you" in request.lower():
+            response = "I am doing well, thank you for asking!"
+        elif "what are you" in request.lower():
+            response = "I'm Ghost, your personal assistant."
+        elif "what is the date" in request.lower():
+            response = f"Today is {datetime.now().strftime('%m/%d/%Y')}"
+        elif "what is the time" in request.lower():
+            response = f"The current time is {datetime.now().strftime('%H:%M:%S')}"
+        elif "what's the weather" in request.lower():
+            # Get weather information from OpenWeatherMap API
+            api_key = 'your api'
+            base_url = "http://api.openweathermap.org/data/2.5/weather?"
+            city_name = "your city"
+            complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+            response = requests.get(complete_url)
+            weather_data = response.json()
+            if weather_data["cod"] != "404":
+                main_data = weather_data["main"]
+                current_temperature = main_data["temp"]
+                current_pressure = main_data["pressure"]
+                current_humidity = main_data["humidity"]
+                weather_desc = weather_data["weather"][0]["description"]
+                response = f"Temperature: {current_temperature}°C\nPressure: {current_pressure} hPa\nHumidity: {current_humidity}%\nWeather description: {weather_desc}"
+            else:
+                response = "City not found"
+        else:
+            response = "I don't understand what you want to say. Can I help you with anything else?"
+    except sr.UnknownValueError:
+        response = "I didn't understand what you said."
+    
+    # Print and speak the response
+    print("Assistant:", response)
+    engine.say(response)
+    engine.runAndWait()
